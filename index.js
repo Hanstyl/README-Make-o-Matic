1933a08fd7169a99c7272dd729e7c3d7596d3f65
Nathan Nelson <nathan.nelson042@gmail.com>
2022-01-02 02:22:59 -0700

    added some of the questions

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your Github user name.',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('You need to enter in you github user name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('You need to enter in a email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'project',
+        message: 'Please enter the name of your project.',
+        validate: projectInput => {
+            if (projectInput) {
+                return true;
+            } else {
+                console.log('Your project needs a name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project.',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Describe your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license',
+        choices: '',
+    },
+];
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
+
+
+
+
+
 
 // Function call to initialize app
 init();
