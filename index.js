0cbc59aa46f14175f8c4b84b78d362ebb5a3a8d8
Nathan Nelson <nathan.nelson042@gmail.com>
2022-01-02 21:39:11 -0700

    added cool ASCII skull on start

--- index.js
+++ index.js
@@ -4,85 +4,139 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 
+
+// TODO: Create a function to start the app
+function start() {
+    console.log(`
+███████████████████████████
+███████▀▀▀░░░░░░░▀▀▀███████
+████▀░░░░░░░░░░░░░░░░░▀████
+███│░░░░░░░░░░░░░░░░░░░│███
+██▌│░░░░░░░░░░░░░░░░░░░│▐██
+██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+██▌░│██████▌░░░▐██████│░▐██
+███░│▐███▀▀░░▄░░▀▀███▌│░███
+██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+████▄─┘██▌░░░░░░░▐██└─▄████
+█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+███████▄░░░░░░░░░░░▄███████
+██████████▄▄▄▄▄▄▄██████████
+███████████████████████████
+   █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+   █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+   █░░║║║╠─║─║─║║║║║╠─░░█
+   █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+   ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+    `);
+
+    return inquirer.prompt([
+        {
+            type: "confirm",
+            name: "start",
+            message: "ReadMe file creator booting up... Would you like to proceed?"
+        }
+    ])
+        .then((answer => {
+            if (answer.start === true) {
+                console.log("Lets-a-go!");
+            } else {
+                console.log("Game Over! Bye");
+                process.exit();
+            }
+        }))
+}
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'githubUser',
-        message: 'Please enter your Github user name.',
-        validate: githubUserInput => {
-            if (githubUserInput) {
-                return true;
-            } else {
-                console.log('You need to enter in you github user name!');
-                return false;
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'githubUser',
+            message: 'Please enter your Github user name.',
+            validate: githubUserInput => {
+                if (githubUserInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter in you github user name!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email address.',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('You need to enter in a email address!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter in a email address!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'project',
-        message: 'Please enter the name of your project.',
-        validate: projectInput => {
-            if (projectInput) {
-                return true;
-            } else {
-                console.log('Your project needs a name!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: 'Please enter the name of your project.',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Your project needs a name!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please describe your project.',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Describe your project!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Describe your project!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Select a license',
-        choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'Unlicense']
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Add installation instructions for the project'
-    },
-    {
-        type: 'input',
-        name: 'githubRepoUrl',
-        message: 'Link to the Repo',
-        validate: githubRepoUrl => {
-            if (githubRepoUrl) {
-                return true;
-            } else {
-                console.log('Please enter a repo link!');
-                return false;
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Select a license',
+            choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'Unlicense']
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Add installation instructions for the project'
+        },
+        {
+            type: 'input',
+            name: 'githubRepoUrl',
+            message: 'Link to the Repo',
+            validate: githubRepoUrl => {
+                if (githubRepoUrl) {
+                    return true;
+                } else {
+                    console.log('Please enter a repo link!');
+                    return false;
+                }
             }
-        }
-    },
-];
+        },
+    ])
+        .then(userData => {
+            return userData;
+        })
+}
 
 
 
@@ -95,15 +149,10 @@ function writeToFile(fileName, data) {
     })
 }
 
-// TODO: Create a function to initialize app
-function init() { 
-    inquirer
-    .prompt(questions)
-    .then((answers) => writeToFile('/dist/readMe.md', answers))
-    .catch((error) => console.log(error))
-}
+
 
 
 
 // Function call to initialize app
+
 init();
